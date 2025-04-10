98a62791cb0e3ad59eecf5817551c187dbde4ae3
Noël TRAN <noeltran@MacBook-Air-de-Noel.local>
2025-04-10 16:16:46 +0200

    add y in the last field

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -6,5 +6,5 @@ select
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
     DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_y
 from {{ source('sales_database', 'order') }}
